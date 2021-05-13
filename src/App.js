7600173f28fd0ab8d1ec03372fca29ac0b32864d
Nathan <nathanmac777@hotmail.com>
2021-05-13 10:58:00 -0700

    phase 1, gitignore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import coffeeBean from "./mockData/coffeeBeans.json";
+import SelectedCoffeeBean from "./components/SelectedCoffeeBean";
+import React from "react";
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <h1>Welcome to Coffee App</h1>
-    </>
+      <SelectedCoffeeBean />
+    </ React.Fragment>
   );
 }
 
--- src/components/SelectedCoffeeBean.js
+++ src/components/SelectedCoffeeBean.js
@@ -1,5 +1,12 @@
+import { useCoffee } from "../context/CoffeeContext";
+
 const SelectedCoffeeBean = () => {
-  return null;
+  const { coffeeBean, setCoffeeBean } = useCoffee();
+  return (
+    <div className="selected-coffee">
+      <h2>{coffeeBean.name}</h2>
+    </div>
+  )
 }
 
 export default SelectedCoffeeBean;
