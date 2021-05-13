32077129195a88304c4a8e7965b6c4873022b0c0
jsalgg <mzootfb@Lukes-MBP.attlocal.net>
2021-05-13 15:32:15 -0400

    final push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import coffeeBean from "./mockData/coffeeBeans.json";
 import SelectedCoffeeBean from "./components/SelectedCoffeeBean";
 import React from "react";
-
+import SetCoffeeBean from "./components/SetCoffeeBean";
 function App() {
   return (
     <React.Fragment>
       <h1>Welcome to Coffee App</h1>
       <SelectedCoffeeBean />
-    </ React.Fragment>
+      <SetCoffeeBean coffeeBeans={coffeeBean} />
+    </React.Fragment>
   );
 }
 
--- src/components/SetCoffeeBean.js
+++ src/components/SetCoffeeBean.js
@@ -1,21 +1,25 @@
+import { useCoffee } from "../context/CoffeeContext";
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const { coffeeBean, setCoffeeBeanId } = useCoffee();
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
+        onChange={(ev) => {
+          setCoffeeBeanId(ev.target.value);
+        }}
+        value={coffeeBean.id}
       >
-        {coffeeBeans.map(bean => (
-          <option
-            key={bean.id}
-            value={bean.id}
-          >
+        {coffeeBeans.map((bean) => (
+          <option key={bean.id} value={bean.id}>
             {bean.name}
           </option>
         ))}
       </select>
     </div>
   );
-}
+};
 
 export default SetCoffeeBean;
